8dc7f846099072419143e5b1769093786810217b
Andrew Hamerly <73661602+andrewhamerly@users.noreply.github.com>
2024-04-29 21:27:56 -0400

    Created a mini render project.

---
+++ routes/diagnostics.js
@@ -0,0 +1,36 @@
+const diagnostics = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving diagnostic information
+diagnostics.get('/', (req, res) => {
+  // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile('./db/diagnostics.json').then((data) => res.json(JSON.parse(data)))
+});
+
+// POST Route for a error logging
+diagnostics.post('/', (req, res) => {
+  // TODO: Logic for appending data to the db/diagnostics.json file
+  const { time, error_id, errors} = req.body;
+
+  if (time && error_id && errors) {
+    const newDisagnostic = {
+      time,
+      error_id: uuidv4,
+      errors
+    }
+
+  readAndAppend(newDisagnostic, './db/diagnostics.json');
+
+  const response = {
+    status: 'success',
+    body: newDisagnostic,
+  };
+
+  res.json(response);
+} else {
+  res.json('Error in posting diagnostics');
+}
+});
+
+module.exports = diagnostics;
